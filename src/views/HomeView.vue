bd9099811990e651e20d40df2976437643adfb6e
ellen <ellen_yu@pixnet.tw>
2023-07-27 18:05:19 +0800

    feat: 加入draggable元件/調整樣式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,45 @@
 <template>
     <main>
-        {{num}}
-        <input type="text" v-model="num">
-
-        <button @click="num++">按鈕</button>
         <div class="container mx-auto p-4 bg-stone-100">
-            <div class="flex justify-between space-x-4">
-                <div class="flex-auto bg-indigo-200 p-5 rounded">233</div>
-                <div class="flex-auto bg-indigo-200 p-5 rounded">343</div>
-                <div class="flex-auto bg-indigo-200 p-5 rounded">好看的</div>
-                <div class="flex-auto bg-indigo-200 p-5 rounded">tar</div>
-            </div>
+            <draggable 
+            v-model="list" 
+            group="people" 
+            @start="drag=true;" 
+            @end="drag=false"
+            @choose="onChoose"
+            @move="onMove"
+            item-key="id"
+            class="flex flex-wrap justify-between gap-y-4">
+                <template #item="{element, index}">
+                    <div class="card flex-initial bg-indigo-200 p-5 rounded" :class="{'selected': index == chosen}">{{element.name}}</div>
+                </template>
+            </draggable>
         </div>
     </main>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-const num = ref(5);
+import draggable from 'vuedraggable'
+//let drag = ref(false);
+const chosen = ref(null);
+const list = ref(
+    [
+      {id: 1, name: 'Item 1'},
+      {id: 2, name: 'Item 2'},
+      {id: 3, name: 'Item 3'},
+      {id: 4, name: 'Item 4'},
+      {id: 5, name: 'Item 5'},
+      {id: 6, name: 'Item 6'},
+    ]
+)
+function onChoose(e) {
+    console.log(e);
+    chosen.value = e.oldIndex;
+}
+
+function onMove(e) {
+    console.log('move', e);
+}
+
 </script>
