c642df2fd7af96d9f6e4ea153484e121000b6d70
ellen <ellen_yu@pixnet.tw>
2023-07-27 14:00:23 +0800

    feat: 新增基本版型

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-  <main>
-    {{num}}
-    <input type="text" v-model="num">
+    <main>
+        {{num}}
+        <input type="text" v-model="num">
 
-    <button @click="num++">按鈕</button>
-  </main>
+        <button @click="num++">按鈕</button>
+        <div class="container mx-auto p-4 bg-stone-100">
+            <div class="flex justify-between space-x-4">
+                <div class="flex-auto bg-indigo-200 p-5 rounded">233</div>
+                <div class="flex-auto bg-indigo-200 p-5 rounded">343</div>
+                <div class="flex-auto bg-indigo-200 p-5 rounded">好看的</div>
+                <div class="flex-auto bg-indigo-200 p-5 rounded">tar</div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script setup>
