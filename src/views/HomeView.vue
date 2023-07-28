8501455b91952f06cf76caf9db6c7e6ac170a46d
ellen <ellen_yu@pixnet.tw>
2023-07-28 13:56:12 +0800

    feat: 調整卡片樣式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,18 @@
         <div class="container mx-auto p-4 bg-stone-100">
             <draggable 
             v-model="list" 
-            group="people" 
+            group="people"
+            animation="400"
             @start="drag=true;" 
             @end="drag=false"
-            @choose="onChoose"
             @move="onMove"
             item-key="id"
             class="flex flex-wrap justify-between gap-y-4">
-                <template #item="{element, index}">
-                    <div class="card flex-initial bg-indigo-200 p-5 rounded" :class="{'selected': index == chosen}">{{element.name}}</div>
+                <template #item="{element}">
+                    <div v-if="element.name" class="card flex-initial bg-indigo-200 p-5 rounded">
+                        <p>{{element.name}}</p>
+                    </div>
+                    <div v-else class="card empty"></div>
                 </template>
             </draggable>
         </div>
@@ -21,7 +24,7 @@
 <script setup>
 import { ref } from 'vue'
 import draggable from 'vuedraggable'
-//let drag = ref(false);
+let drag = ref(false);
 const chosen = ref(null);
 const list = ref(
     [
@@ -31,12 +34,10 @@ const list = ref(
       {id: 4, name: 'Item 4'},
       {id: 5, name: 'Item 5'},
       {id: 6, name: 'Item 6'},
+      {},
+      {}
     ]
 )
-function onChoose(e) {
-    console.log(e);
-    chosen.value = e.oldIndex;
-}
 
 function onMove(e) {
     console.log('move', e);
